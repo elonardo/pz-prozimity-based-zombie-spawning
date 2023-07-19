2a4c29cf10722e819bcda3bd832b9db1c6ddac4e
Leonardo Patmore-Zarcone <elonardo@gmail.com>
2023-07-19 17:34:07 -0400

    update add heat

--- media/lua/server/pbzs_spawn.lua
+++ media/lua/server/pbzs_spawn.lua
@@ -44,47 +44,46 @@ end
 local function pbzs_add_heat(pbzs_player)
     local player_cell, player_x, player_y, player_z = pbzs_get_player_xyz(pbzs_player)
     
+    --get cell coordinates from square
+    player_x = math.floor(player_x/300)
+    player_y = math.floor(player_y/300)
+
     --get points around the players location to add heat to
-    local add_10 = {player_x,player_y}
-    local add_05 = {{player_x+300,player_y},{player_x-300,player_y},{player_x,player_y+300},{player_x,player_y-300}}
-    local add_03 = {{player_x+300,player_y+300},{player_x-300,player_y-300},{player_x-300,player_y+300},{player_x+300,player_y-300},{player_x+600,player_y},{player_x-600,player_y},{player_x,player_y+600},{player_x,player_y-600}}
-    local add_02 = {{player_x+600,player_y+300},{player_x-600,player_y-300},{player_x-600,player_y+300},{player_x+600,player_y-300},{player_x-300,player_y+600},{player_x+300,player_y-600},{player_x+300,player_y+600},{player_x-300,player_y-600}}
-    local add_01 = {{player_x+600,player_y+600},{player_x-600,player_y-600},{player_x-600,player_y+600},{player_x+600,player_y-600}}
+    local add_02 = {{player_x,player_y},{player_x+1,player_y},{player_x-1,player_y},{player_x,player_y+1},{player_x,player_y-1},{player_x+1,player_y+1},{player_x-1,player_y-1},{player_x-1,player_y+1},{player_x+1,player_y-1}}
+    local add_01 = {{player_x+2,player_y},{player_x-2,player_y},{player_x,player_y+2},{player_x,player_y-2},{player_x+2,player_y+1},{player_x-2,player_y-1},{player_x-2,player_y+1},{player_x+2,player_y-1},{player_x-1,player_y+2},{player_x+1,player_y-2},{player_x+1,player_y+2},{player_x-1,player_y-2},{player_x+2,player_y+2},{player_x-2,player_y-2},{player_x-2,player_y+2},{player_x+2,player_y-2}}
 
     --adds heat in the following pattern:
-    --01,02,03,02,01
-    --02,03,05,03,02
-    --03,05,10,05,03
-    --02,03,05,03,02
-    --01,02,03,02,01
-    --at max 10 heat per zombie and 2 decay per hour, this creates 25 zombies the first hour, 13 the second, 5 the third, 1 the fourth and fifth for a total of 45 zombies over 5 hours
-    --at these settings, if you do not kill an average of 45 zombies per hour, the total number of zombies in the world will always increase
-    --really need to get the cell level heat map (instead of square) working so heat can be concentrated.
+    --01,01,01,01,01
+    --01,02,02,02,01
+    --01,02,02,02,01
+    --01,02,02,02,01
+    --01,01,01,01,01
 
-    pbzs_heatmap[add_10] = 10
-    for key, value in ipairs(add_05) do
-        pbzs_heatmap[value] = 5
-    end
-    for key, value in ipairs(add_03) do
-        pbzs_heatmap[value] = 3
-    end
     for key, value in ipairs(add_02) do
-        pbzs_heatmap[value] = 2
+        pbzs_heatmap[value] = (value + 2 or 2)
     end
     for key, value in ipairs(add_01) do
-        pbzs_heatmap[value] = 1
+        pbzs_heatmap[value] = (value + 1 or 1)
     end
-    return player_cell
+    return player_x, player_y
 end
 
 local function pbzs_main()
-    --every hour, get the players location, add heat to the area around the player, and spawns zombies everywhere there's heat
+    
+    local dont_spawn = {}
+
+    --every hour, get the players location, add heat to the area around the player, and add players location to the don't spawn cell
     for pbzs_playerindex = 0, getNumActivePlayers() - 1 do
         local pbzs_player = getSpecificPlayer(pbzs_playerindex)
-        local player_cell = pbzs_add_heat(pbzs_player)
-        pbzs_spawn()
+        local player_x, player_y = pbzs_add_heat(pbzs_player)
+        table.insert(dont_spawn, {player_x, player_y})
     end
-    print("success!")
+
+    --every hour, spawn zombies
+    for pbzs_playerindex = 0, getNumActivePlayers() - 1 do
+        pbzs_spawn(dont_spawn)
+    end
+    print("proximity based zombie spawning succesful")
 end
 
 Events.EveryHours.Add(pbzs_main)
